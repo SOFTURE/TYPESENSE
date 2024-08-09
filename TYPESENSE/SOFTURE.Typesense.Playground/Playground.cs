d7f733ae455959f0c4af705183884b14c5c6b9f1
Jarosław Armatys <jarekarmatys@gmail.com>
2024-08-09 17:23:52 +0200

    Configured Bogus for data generation

--- TYPESENSE/SOFTURE.Typesense.Playground/Playground.cs
+++ TYPESENSE/SOFTURE.Typesense.Playground/Playground.cs
@@ -1,3 +1,4 @@
+using Bogus;
 using CSharpFunctionalExtensions;
 using SOFTURE.Typesense.Abstractions;
 using SOFTURE.Typesense.Playground.Examples;
@@ -8,18 +9,22 @@ public sealed class Playground(IDocumentClient documentClient)
 {
     public async Task Run()
     {
-        var exampleDocument = new ExampleDocument
-        {
-            Id = "1",
-            ClientId = "1",
-            Name = "John Doe",
-            Identifier = "JD",
-            City = "New York",
-        };
+        var currentId = 1;
+        var exampleDocuments = new Faker<ExampleDocument>()
+            .StrictMode(true)
+            .RuleFor(o => o.Id, f => (currentId++).ToString())
+            .RuleFor(o => o.Name, f => f.Name.FullName())
+            .RuleFor(o => o.Identifier, f => f.Random.String2(2))
+            .RuleFor(o => o.City, f => f.Address.City())
+            .Generate(50);
 
-        await documentClient.UpsertDocument(exampleDocument)
-            .Tap(() => Console.WriteLine("Document upserted"))
-            .TapError(error => Console.WriteLine($"Error creating document: {error}"));
+        foreach (var exampleDocument in exampleDocuments)
+        {
+            await documentClient.UpsertDocument(exampleDocument)
+                .Tap(() => Console.WriteLine("Document upserted"))
+                .TapError(error => Console.WriteLine($"Error creating document: {error}"));
+        }
+
 
         var exampleQuery = new ExampleQuery
         {
