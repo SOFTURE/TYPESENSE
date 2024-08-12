6648a82d18f1757f7aed14437bb1550d953f6848
Jarosław Armatys <jarekarmatys@gmail.com>
2024-08-12 17:02:07 +0200

    Added support for .NET6

--- TYPESENSE/SOFTURE.Typesense/Models/DocumentBase.cs
+++ TYPESENSE/SOFTURE.Typesense/Models/DocumentBase.cs
@@ -1,4 +1,3 @@
-
 using SOFTURE.Typesense.ValueObjects;
 
 namespace SOFTURE.Typesense.Models;
@@ -6,5 +5,12 @@ namespace SOFTURE.Typesense.Models;
 public abstract class DocumentBase(Collection collection)
 {
     public Collection Collection => collection;
+
+#if NET8_0
     public abstract required string Id { get; set; }
+#endif
+
+#if NET6_0
+    public abstract string Id { get; set; }
+#endif
 }
--- TYPESENSE/SOFTURE.Typesense/Settings/TypesenseSettings.cs
+++ TYPESENSE/SOFTURE.Typesense/Settings/TypesenseSettings.cs
@@ -2,7 +2,15 @@ namespace SOFTURE.Typesense.Settings;
 
 public sealed class TypesenseSettings
 {
+#if NET8_0
     public required string Host { get; init; }
     public required string Port { get; init; }
     public required string ApiKey { get; init; }
+#endif
+
+#if NET6_0
+    public string Host { get; init; } = null!;
+    public string Port { get; init; } = null!;
+    public string ApiKey { get; init; } = null!;
+#endif
 }
